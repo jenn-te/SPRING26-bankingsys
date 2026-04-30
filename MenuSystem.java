70e12522ffeb8368aa0d0f9499b614ecdf0f3e38
Rafah8 <rafah.alsalkini@owl.ucc.edu>
2026-04-30 10:50:44 -0400

    Add files via upload

---
+++ MenuSystem.java
@@ -0,0 +1,84 @@
+package finalProject;
+import java.util.Scanner;
+public class MenuSystem {
+    static Scanner input=new Scanner(System.in);
+
+       public static void main(String[]args){
+        
+
+        int choice;
+        
+        do{
+        System.out.println("\n===========================");
+        System.out.println("     Banking System Menu");
+        System.out.println("===========================");
+        System.out.println("1. Create Account");
+        System.out.println("2. Deposit Money");
+        System.out.println("3. Withdraw Money");
+        System.out.println("4. Check Balance");
+        System.out.println("5. Exit");
+        System.out.println("Enter your choice: ");
+            choice=getValidChoice();
+            switch(choice){
+                case 1:
+                    createAccount();
+                    break;
+                case 2:
+                    depositMoney();
+                    break;
+                case 3:
+                    withdrawMoney();
+                    break;
+                case 4:
+                    checkBalance();
+                    break;
+                case 5:
+                    System.out.println("Thank you for using the bank system. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again. ");
+                
+
+            }
+            
+        }while(choice!=5);
+
+    }
+
+     
+        
+    
+
+    public static int getValidChoice(){
+        int choice=-1;
+
+        while(true){
+            if(input.hasNextInt()){
+                choice=input.nextInt();
+
+                if (choice>=1 && choice <=5){
+                    return choice;
+                }else{
+                    System.out.println("Choice must be between 1-5. Try again");
+                }
+            }else{
+                System.out.println("invalid input. Enter a number: ");
+                input.next();
+            }
+            }
+            
+
+    }
+    public static void createAccount(){
+        System.out.println("\nCreate Account ");
+    }
+    public static void withdrawMoney(){
+        System.out.println("\nWithdraw Money");
+    }
+    public static void checkBalance(){
+        System.out.println("\nCheck Balance");
+    }
+    public static void depositMoney(){
+        System.out.println("\nDeposit money");
+    }
+}

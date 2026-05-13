833a9ebad96d1e34d3ed3cba8897e7b8f1e28c5b
michaelcastellanos09 <mochaelcastellanos@gmail.com>
2026-05-13 01:32:48 -0400

    Add balance checking functionality
    Implemented balance checking logic in checkBalance class.

--- checkBalance.java
+++ checkBalance.java
@@ -1,8 +1,25 @@
-// logic for checking balance here
-
+//michael
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class checkBalance {
+
     public static void run(ArrayList<Account> accounts) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter account number: ");
+        int accNum = input.nextInt();
+
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getAccNum() == accNum) {
+                System.out.println("Account holder: " + accounts.get(i).getName());
+                System.out.println("Balance: $" + accounts.get(i).getBalance());
+                return;
+            }
+        }
+        System.out.println("Account not found.");
+    }
+
+    public static double getBalance(int accNum) {
+        return 0;
     }
 }
